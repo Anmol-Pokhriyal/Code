3432f001583633e7530e1f9c2601e55f806d2311
Anmol-Pokhriyal <84918092+Anmol-Pokhriyal@users.noreply.github.com>
2022-01-25 20:50:50 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1> My favroute food</h1>
+    <ul>
+      <li>Fruites</li>
+    </ul>
+    <ul>
+      <li>Fruites</li>
+    </ul>
+    <ul>
+      <li>Fruites</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
